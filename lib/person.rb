211105c6506a3df2b2bd9dc9215052adf0c59e8a
calvin <calvin.francis96@outlook.com>
2021-07-08 16:30:17 -0400

    Done with assignment.

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+calvin = Person.new
+calvin.talk
+calvin.walk
